f2985fe71626e62fc3bcaf8853c5e4e7366cb0b7
Your Name <you@example.com>
2022-08-25 18:52:24 +0200

    komit

---
+++ README.md
@@ -0,0 +1,3 @@
+# projekty
+# projekty
+"# projekty" 
---
+++ main.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import json, request
+from flask import Flask, jsonify
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return jsonify({'name': 'alice',
+                    'email': 'alice@outlook.com'})
+
+@app.route('/user/request', methods=['PUT'])
+def create_record():
+    record = json.loads(request.data)
+    with open('/tmp/data.txt', 'r') as f:
+        data = f.read()
+    if not data:
+        records = [record]
+    else:
+        records = json.loads(data)
+        records.append(record)
+    with open('/tmp/data.txt', 'w') as f:
+        f.write(json.dumps(records, indent=2))
+    return jsonify(record)
+
+
+# @app.route('/maschine', methods=['POST'])
+# def create_record():
+#     record = json.loads(request.data)
+#     with open('/tmp/data.txt', 'r') as f:
+#         data = f.read()
+#     if not data:
+#         records = [record]
+#     else:
+#         records = json.loads(data)
+#         records.append(record)
+#     with open('/tmp/data.txt', 'w') as f:
+#         f.write(json.dumps(records, indent=2))
+#     return jsonify(record)
+
+app.run()
